19307f78ccb5907ad60f95cd65e1afd6a527f56e
Abby Sheridan <abbypsheridan@gmail.com>
2025-07-09 14:14:29 -0700

    edit

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def trigger_update():
     return "Metafield updates complete!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+
